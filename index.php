c6c17068e21f8877ce129ed9359ee0c2199183c7
shreya22042001 <87891109+shreya22042001@users.noreply.github.com>
2021-09-21 22:29:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include 'includes/common.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Basic Banking System </title>
+</head>
+
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="content" style="background-image: url('https://image.shutterstock.com/image-photo/busines…an-holding-word-banking-hand-260nw-1150180799.jpg');">
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
